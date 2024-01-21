ca4c2100407965b779b1ef4c8dd31988a8daa709
JSH <skyjs135@gmail.com>
2024-01-21 21:18:09 +0900

    name password email nickname 변수 추가

--- src/main/java/hello/productservice/main/data/entity/Member.java
+++ src/main/java/hello/productservice/main/data/entity/Member.java
@@ -1,12 +1,32 @@
 package hello.productservice.main.data.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Entity
+@Getter
+@RequiredArgsConstructor
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)//auto_increment 자동생성
     @Column(name = "member_id")
     private Long memberId;
+    private String memberName;
+
+    private String memberPassword;
+
+    private String memberEmail;
+
+
+    private String memberNickName;
+
+    public void saveMember(String memberName, String memberEmail,String memberPassword,String memberNickName){
+        this.memberName=memberName;
+        this.memberPassword=memberPassword;
+        this.memberEmail=memberEmail;
+        this.memberNickName=memberNickName;
+
+    }
 
 }
