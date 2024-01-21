7af10f47cecc9a7478708f46090f0eea2ab02062
JSH <skyjs135@gmail.com>
2024-01-21 21:19:10 +0900

    MemberDto 추가

---
+++ src/main/java/hello/productservice/main/data/dto/MemberDto.java
@@ -0,0 +1,20 @@
+package hello.productservice.main.data.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class MemberDto {
+    private Long memberId;
+
+    private String memberName;
+
+    private String memberPassword;
+
+    private String memberEmail;
+
+    private String memberNickName;
+}
