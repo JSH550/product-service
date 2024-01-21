1022b279f706dba848629ba70afa8c8e5f245c02
JSH <skyjs135@gmail.com>
2024-01-21 21:20:45 +0900

    MemberService interface 추가

---
+++ src/main/java/hello/productservice/main/service/MemberService.java
@@ -0,0 +1,10 @@
+package hello.productservice.main.service;
+
+import hello.productservice.main.data.dto.MemberDto;
+import hello.productservice.main.data.entity.Member;
+
+public interface MemberService {
+
+
+    MemberDto saveMember(MemberDto memberDto);
+}
