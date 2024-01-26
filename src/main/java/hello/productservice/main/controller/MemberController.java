c4787c867286854f78f77c3d79aeaf03f238b6a7
JSH <skyjs135@gmail.com>
2024-01-26 14:58:28 +0900

    로그인 회원정보 저장 cookie 방식에서 session 방식으로 변경

--- src/main/java/hello/productservice/main/controller/MemberController.java
+++ src/main/java/hello/productservice/main/controller/MemberController.java
@@ -1,6 +1,7 @@
 package hello.productservice.main.controller;
 
 import hello.productservice.main.data.dto.MemberDto;
+import hello.productservice.main.data.entity.Member;
 import hello.productservice.main.service.MemberService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,14 +11,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
+import java.util.Optional;
 
 @Slf4j
 @Controller
 @RequestMapping("/members")
-@SessionAttributes
+//@SessionAttributes
 public class MemberController {
 
   private final MemberService memberService;
@@ -43,10 +46,13 @@ public class MemberController {
     //회원가입
     @ResponseBody
     @PostMapping("")
-    public String addNewMember(@ModelAttribute("memberDto") MemberDto memberDto){
-        log.info("결과값");
-        log.info(String.valueOf(memberDto.getMemberName()));
-        log.info(String.valueOf(memberDto.getMemberPassword()));
+    public String addNewMember(@ModelAttribute("memberDto") MemberDto memberDto,
+                               HttpServletRequest request){
+//        log.info("결과값");
+//        log.info(String.valueOf(memberDto.getMemberName()));
+//        log.info(String.valueOf(memberDto.getMemberPassword()));
+
+
 
         MemberDto savedMember = memberService.saveMember(memberDto);
         return savedMember.getMemberName();
@@ -58,117 +64,106 @@ public class MemberController {
     public String showLoginPage(){
         return "/member/loginForm";
     }
-    @ResponseBody
+
+
     @PostMapping("/login")
     public String login(@RequestParam String memberName,
-                        @RequestParam String password){
+                        @RequestParam String password,
+                        HttpServletResponse response,
+                        HttpServletRequest request) {
 
-        //DB에서 id password 조회해서
-        //있으면 홈페이지로 보내주고
-        //없으면 로그인 에러 메시지 보내줘
-        //작동함
-        MemberDto foundMemberByName = memberService.findMemberByName(memberName);
-        log.info("결과");
-        log.info(password);
-        log.info(foundMemberByName.getMemberPassword());
-        if(foundMemberByName == null){
-            log.info("값이없음");
-            return "아이디를 확인하세요";
+        Optional<MemberDto> loginMember = memberService.login(memberName, password);
+        if (loginMember.isEmpty()==true){
+            log.info("로그인 에러 검출");
+            return "/member/loginForm";
+        }
+        //세션+쿠키(토큰) 방식으로 로그인 유저 저장
+        //getSession(true) 세션이 없으면 만들고 있으면 반환
+        //getSession(false) 세션이 없으면 새로만들지 않음
+        HttpSession session = request.getSession(true);
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute("LOGIN_MEMBER",loginMember.get().getMemberName());
+        log.info("Session LOGIN_MEMBER: {}", session.getAttribute("LOGIN_MEMBER"));
 
-        }else if(Objects.equals(foundMemberByName.getMemberPassword(), password)) {
-            return "로그인성공";
+//        return "redirect:/members/mypage";
+        return "redirect:/members";
 
+    };
+
+
+    //(@SessionAttribute(name ="LOGIN_MEMBER",required = false session에 있는지 확인, 새로생성하진 않음
+    @GetMapping("/mypage")
+    public String mypage(@SessionAttribute(name ="LOGIN_MEMBER",required = false) MemberDto loginMember
+                        ,HttpServletRequest request,
+                         Model model){
+        if(loginMember==null){
+            log.info("로그인 하지 않은 사용자 login page로 redirect");
+            return "redirect:/members/login";
         }else {
-            return "비밀번호가 틀렸습니다.";
+
+            log.info("login member, mypage로 redirect");
+            log.info(String.valueOf(loginMember));
+
+            model.addAttribute("member",loginMember);
+            return "/member/memberMyPage";
         }
-        }
-
-        //작동함
-//        MemberDto memberByName = memberService.findMemberById(1L);
-//        log.info(id);
-//            log.info(password);
-//            log.info(String.valueOf(memberByName.getMemberId()));
 
 
 
 
-//    @GetMapping("/test")
-//    public String testPage(HttpServletResponse response){
-//
-//        Cookie cookie = new Cookie("testName", "testValue");
-//        cookie.setMaxAge(60 * 1); // 쿠키의 유효 시간 (초 단위)
-//        cookie.setPath("/");
-//        response.addCookie(cookie);
-//            return "redirect:/";
-//    }
 
-    //db find member test
-    @ResponseBody
-    @GetMapping("/test3")
-    public String test3(){
-//        MemberDto memberDto = new MemberDto();
-//        memberDto.setMemberId(1L);
-//        memberDto.setMemberName("test1");
-//        memberDto.setMemberPassword("1234");
 
-//        MemberDto memberByName = memberService.findMemberById(memberDto);
-        MemberDto memberByName = memberService.findMemberByName("dd");
-        return memberByName.getMemberPassword();
+
+//        memberId.equals(null) 값이 null일경우 equals호출할수 없음 Objects.equlas(memberId,null)이렇게 하자
+        /*
+        mypage 접속시 cookie 확인, 없거나 DB에서 조회가 안되면 로그인 페이지로 리다이렉트
+         */
+//        if ( Objects.equals(memberId, null)){
+//            log.info("로그인 하지 않은 사용자");
+//        return "redirect:/members/login";
+//       }else if(memberEmptyCheck(memberId)) {
+//            log.info("등록되지 않은 사용자 접속");
+//            return "redirect:/members/login";
+//        }else {
+//            model.addAttribute("memberId",memberId);
+//            return "/member/memberMyPage";
+//        }
 
 
     };
 
-    @GetMapping("/test")
-    public String login(HttpServletRequest request,
-                        HttpServletResponse response,
-                        HttpSession session){
-
-        session.setAttribute("sessionTest", "sTestValue");
-        session.setMaxInactiveInterval(600);
-
-        log.info((String) session.getAttribute("sessionTest"));
-        System.out.println("SessionTest Value: " + session.getAttribute("sessionTest"));
-        Cookie cookie = new Cookie("testName", "testValue");
-        cookie.setMaxAge(60 * 1); // 쿠키의 유효 시간 (초 단위)
-        cookie.setPath("/");
-        response.addCookie(cookie);
-        return "redirect:/";
+    public boolean memberEmptyCheck(String memberId){
+        Optional<MemberDto> memberByName = Optional.ofNullable(memberService.findMemberByName(memberId));
+        if (memberByName.isEmpty()==true){
+            return true;
+        }else {
+            return false;
+        }
     }
 
-    @GetMapping("/test2")
-    public String login2(HttpServletRequest request,
-                        HttpServletResponse response,
-                        HttpSession session){
-        log.info((String) session.getAttribute("sessionTest"));
-        System.out.println("SessionTest Value: " + session.getAttribute("sessionTest"));
-        return "redirect:/";
+//    @GetMapping("/logout")
+//    public String lougOut(HttpServletResponse response){
+//        Cookie cookie = deleteCookie("memberId");
+//        response.addCookie(cookie);
+//        return "redirect:/members";
+//    }
+
+    @GetMapping("/logout")
+    public String lougOut(HttpServletRequest request,
+            HttpServletResponse response){
+        HttpSession session = request.getSession(false);
+        if(session!=null){
+            //session 정보 삭제
+            session.invalidate();
+        }
+
+        return "redirect:/members";
     }
-
-
-    @ResponseBody
-    @GetMapping("/createSession")
-    public String createSession(HttpSession session, HttpServletResponse response) {
-        // 세션 생성 및 데이터 설정
-        session.setAttribute("username", "john_doe");
-        // 쿠키 생성 및 설정
-        Cookie cookie = new Cookie("sessionId", session.getId());
-//        cookie.setMaxAge(60 * 60); // 쿠키의 유효 시간 (초 단위)
-        cookie.setPath("/"); // 쿠키의 경로 설정
-        response.addCookie(cookie);
-        return "ok";
-
-//            return "redirect:/";
-    }
-
-
-    @ResponseBody
-    @GetMapping("/getSessionAttribute")
-    public String getSessionAttribute(HttpServletRequest request) {
-        // 세션에서 속성 가져오기
-        HttpSession session = request.getSession();
-        String username = (String) session.getAttribute("username");
-        System.out.println("Username from session: " + username);
-        return "ok";
+    public Cookie deleteCookie(String cookieName){
+        //cookie age를 0으로 바꿔 삭제
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        return cookie;
     }
 }
 
