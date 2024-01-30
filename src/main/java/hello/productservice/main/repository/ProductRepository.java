976326e52b9a1be66586be66c49e89a3bed8c1ba
JSH <skyjs135@gmail.com>
2024-01-30 22:20:17 +0900

    keyword를 포함한 productName 조회하는 기능 구현

--- src/main/java/hello/productservice/main/repository/ProductRepository.java
+++ src/main/java/hello/productservice/main/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package hello.productservice.main.repository;
 import hello.productservice.main.data.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -10,5 +11,6 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
    Optional<Product> findByProductName(String productName);
    boolean existsByProductName(String productName);
 //    void deleteByProductName(String productName);
+   List<Product> findByProductNameContaining(String searchKeyword);
 
 }
