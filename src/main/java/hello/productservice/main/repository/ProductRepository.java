08a44c25e23b7d64a02f29ab302fc79567877458
JSH <skyjs135@gmail.com>
2024-02-12 23:56:19 +0900

    검색어를 받아서 DB에서 조회하는 로직 추가

--- src/main/java/hello/productservice/main/repository/ProductRepository.java
+++ src/main/java/hello/productservice/main/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package hello.productservice.main.repository;
 
-import hello.productservice.main.data.entity.Product;
+import hello.productservice.main.data.entity.product.Product;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +13,10 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
    Optional<Product> findByProductName(String productName);
    boolean existsByProductName(String productName);
 //    void deleteByProductName(String productName);
+
+//   searchKeyword를 포함하는 요소 검색
+   List<Product> findByProductNameContaining(String searchKeyword, PageRequest pageRequest);
    List<Product> findByProductNameContaining(String searchKeyword);
+   Long countByProductNameContaining(String searchKeyword);
 
 }
