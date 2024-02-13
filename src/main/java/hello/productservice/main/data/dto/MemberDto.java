2dd778925636d553ee5d5718168d654bfd8c2eea
JSH <skyjs135@gmail.com>
2024-02-13 23:55:16 +0900

    bean validation 검증 로직 추가

--- src/main/java/hello/productservice/main/data/dto/MemberDto.java
+++ src/main/java/hello/productservice/main/data/dto/MemberDto.java
@@ -1,5 +1,7 @@
 package hello.productservice.main.data.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -10,10 +12,12 @@ import lombok.Setter;
 public class MemberDto {
     private Long memberId;
 
+    @Email(message = "올바른 이메일 형식이어야 합니다.")
+    @NotBlank(message = "이메일을 입력해주세요.")
     private String memberEmail;
-
+    @NotBlank(message = "이름을 입력해주세요.")
     private String memberName;
-
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String memberPassword;
 
 //    private String memberEmail;
