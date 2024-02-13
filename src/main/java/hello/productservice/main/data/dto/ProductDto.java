64d17a6c48853adf8c6d3057d310ead6ee3d0e23
JSH <skyjs135@gmail.com>
2024-02-13 23:42:13 +0900

    bean validation 검증 로직 추가

--- src/main/java/hello/productservice/main/data/dto/ProductDto.java
+++ src/main/java/hello/productservice/main/data/dto/ProductDto.java
@@ -1,5 +1,8 @@
 package hello.productservice.main.data.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -13,9 +16,14 @@ import java.util.List;
 public class ProductDto {
 
     private Long productId;
+
+    @NotBlank(message = "제조사를 입력해주세요.")
     private String productManufacturer;
+    @NotBlank(message = "제품명을 입력해주세요.")
     private String productName;
+    @Min(value = 0, message = "가격은 0보다 커야합니다.")
     private double productPrice;
+    @Min(value = 0, message = "수량은 0보다 커야합니다.")
     private int productQuantity;
 
 //    private List<String> productImagesPath;
