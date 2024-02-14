641e3566003eac5b6f541ed586966d4eb7c3f741
JSH <skyjs135@gmail.com>
2024-02-14 23:55:39 +0900

    productDto productId @NotBlank 추가, 범용DTO로 사용(saveDTO 따로 분리)

--- src/main/java/hello/productservice/main/data/dto/ProductDto.java
+++ src/main/java/hello/productservice/main/data/dto/ProductDto.java
@@ -1,5 +1,6 @@
 package hello.productservice.main.data.dto;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -14,17 +15,19 @@ import java.util.List;
 @Getter
 @Setter
 public class ProductDto {
-
+    @NotBlank
     private Long productId;
-
     @NotBlank(message = "제조사를 입력해주세요.")
     private String productManufacturer;
     @NotBlank(message = "제품명을 입력해주세요.")
     private String productName;
     @Min(value = 0, message = "가격은 0보다 커야합니다.")
-    private double productPrice;
-    @Min(value = 0, message = "수량은 0보다 커야합니다.")
-    private int productQuantity;
+    @Max(value = 100000, message = "가격은 100,000원 보다 작아야합니다.")
+    private int productPrice;
+
+//    @Min(value = 0, message = "수량은 0보다 커야합니다.")
+//    @Max(value = 100000, message = "수량은 100000보다 작아야합니다.")
+//    private int productQuantity;
 
 //    private List<String> productImagesPath;
     private List<String> productImagesName;
