a9417cddcc1367b92b0bf123032aa3237eab6dba
JSH <skyjs135@gmail.com>
2024-02-18 23:51:47 +0900

    product 검색 api 예외처리 추가

--- src/main/java/hello/productservice/main/controller/ProductController.java
+++ src/main/java/hello/productservice/main/controller/ProductController.java
@@ -1,10 +1,12 @@
 package hello.productservice.main.controller;
 
-import hello.productservice.main.data.dto.ProductDto;
+import hello.productservice.main.data.dto.product.ProductDto;
+import hello.productservice.main.data.dto.product.ProductPageInfoDto;
+import hello.productservice.main.data.dto.product.ProductSaveDto;
 import hello.productservice.main.service.ProductService;
+import hello.productservice.main.validator.ValidationUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.buf.UriUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -15,6 +17,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriUtils;
@@ -23,42 +27,24 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.UUID;
+import java.util.*;
 
 @Slf4j
 @Controller
 @RequestMapping("/products")
+
 public class ProductController {
 
+    private final ProductService productService;
     //uploadDirectory, application.properties에서의 property값 가져옴
     @Value("${file.dir}")
     private String uploadDirectory;
-
-    private final ProductService productService;
+//    private final ProductValidator productValidator;
 
     @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
-    }
-
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleIllegalArgumentException(Model model, IllegalArgumentException ex) {
-        model.addAttribute("customErrorMessage", "Bad Request: " + ex.getMessage());
-        return "customError";
-    }
-
-    @ExceptionHandler(NoSuchElementException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleNoSuchElementException(Model model, NoSuchElementException ex) {
-        model.addAttribute("customErrorMessage", "Bad Request: " + ex.getMessage());
-        return "customError";
+//        this.productValidator = productValidator;
     }
 
 //    @GetMapping("")
@@ -72,90 +58,112 @@ public class ProductController {
 //
 //    };
 
-//    @ResponseBody
+    //클라이언트가 요청한 product 갯수, pageNumber로 produtList page 보여주는 기능
+    //    @ResponseBody
     @GetMapping("")
-    public String showProductListByPage(@RequestParam Integer pageNumber,
-                                        @RequestParam Integer listSize,
-                                        Model model){
-
-
+    public String showProductListByPage(@RequestParam(required = false) Integer pageNumber,
+                                        @RequestParam(required = false) Integer listSize,
+                                        Model model) {
         // 클라이언트가 요청한 페이지 번호와 리스트 크기를 그대로 사용
         // 단, 리스트 크기는 최소 4 최대 100으로 제한
-        if(pageNumber==null || pageNumber==0){
-            pageNumber=1;//페이지 번호가 null이거나 0일경우 기본값 1로 설정
-        }
-
-        if (listSize ==null || listSize==0 ){
-            listSize=4;//리스트 크기가 null이거나 0일경우 기본값 4로 설정
-        } else if (listSize >100) {
-            listSize=100;//리스트 크기가 100을 초과할경우 100으로 제한
-
-        }
+        pageNumber = ValidationUtils.validatePageNumber(pageNumber);
+        listSize = ValidationUtils.validateListSize(listSize);
 
         List<ProductDto> productList = productService.findProducts(pageNumber, listSize);
+        //DB에 저장된 product 전체 수/listSize, 소수점은 올림처리
+        int maxPageSize = productService.calculateMaxPageSize(listSize);
 
         model.addAttribute("productList", productList);
-        model.addAttribute("pageNumber",pageNumber);
-        model.addAttribute("listSize",listSize);
+        model.addAttribute("pageNumber", pageNumber);
+        model.addAttribute("listSize", listSize);
+        model.addAttribute("maxPageSize", maxPageSize);
 
         ProductDto productDto = productList.get(0);
         log.info(productDto.getProductName());
         return "/product/product-list-new2";
-//        log.info("pageNumber={}",pageNumber);
-//        log.info("listSize={}",listSize);
-//        return "ok";
-    };
+    }
 
+//    검색기능, 클라이언트가 검색어를 입력하면 query parameter로 받고 DB에서 Like로 조회
 
-
-
-
-    /*/
-    검색기능, 클라이언트가 검색어를 입력하면 query parameter로 받고 DB에서 Like로 조회
-     */
-
+    //    @ResponseBody
     @GetMapping("/search")
-    public String searchProduct(@RequestParam String query,
-                                Model model){
-        List<ProductDto> searchedProductList = productService.searchProducts(query);
-        model.addAttribute("productList",searchedProductList);
-        log.info(query);
-        return "/product/product-list";
+    public String searchProduct(@RequestParam(required = false) Integer pageNumber,
+                                @RequestParam(required = false) Integer listSize,
+                                @RequestParam(required = false) String query,
+                                Model model) {
 
+        //query에 아무것도 담겨있지 않으면 예외로 던짐
+        if (query.isEmpty()) {
+            throw new IllegalArgumentException("잘못된 요청입니다.");
+        }
+
+        // 클라이언트가 요청한 페이지 번호와 리스트 크기를 그대로 사용
+        // 단, listSIze min 4 ~ max 100
+        pageNumber = ValidationUtils.validatePageNumber(pageNumber);
+        listSize = ValidationUtils.validateListSize(listSize);
+
+        List<ProductDto> productList = productService.findProducts(pageNumber, listSize, query);
+
+        int maxPageSize = productService.calculateMaxPageSize(listSize, query);
+
+        ProductPageInfoDto productPageInfoDto = new ProductPageInfoDto();
+        productPageInfoDto.saveProductListDto(pageNumber,maxPageSize,listSize);
+
+        model.addAttribute("productPageInfoDto", productPageInfoDto);
+        model.addAttribute("productList", productList);
+//        model.addAttribute("pageNumber", pageNumber);
+//        model.addAttribute("listSize", listSize);
+//        model.addAttribute("maxPageSize", maxPageSize);
+
+//        List<ProductDto> searchedProductList = productService.searchProducts(query);
+//
+//        model.addAttribute("productList",searchedProductList);
+
+        return "/product/product-list-new2";
+//        return "ok";
     }
 
     //새로운 prodcut 등록시, DB에 중복된 이름이 있으면 에러페이지로 이동
 
 
     @GetMapping("/new")
-    public String showProductAddForm(Model model){
-
-        model.addAttribute("productDto",new ProductDto());
+    public String showProductAddForm(Model model) {
+//        model.addAttribute("productDto",new ProductDto())
+        model.addAttribute("productSaveDto", new ProductSaveDto());
         return "/product/product-addform";
-
-    };
-    @ResponseBody
-    @PostMapping("")
-    public String addNewProduct(@ModelAttribute("productDto") ProductDto productDto,
-                                @RequestParam("productImages") List<MultipartFile> productImages) {
-        log.info("productName={}",productDto.getProductName());
-        log.info("productPrice={}",productDto.getProductPrice());
-        log.info("productQuantity={}",productDto.getProductQuantity());
-        log.info("업로드위치={}",uploadDirectory);
-
-
-        try {
-            ProductDto productDto1 = productService.saveProduct(productDto,productImages);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return"ok";
-
-        //DB에서 상품명으로 조회, true false 반환
-        //true면 에러 반환 flase면 save 진행
-//        ProductDto productDto1 = productService.saveProduct(productDto);
     }
 
+    ;
+
+    //    새로운 Product 추가 기능
+//    @ResponseBody
+    @PostMapping("/new")
+    public String addNewProduct(@Validated @ModelAttribute("productSaveDto") ProductSaveDto productSaveDto,
+                                BindingResult bindingResult, Model model,
+                                @RequestParam("productImages") List<MultipartFile> productImages) throws IOException {
+
+//        log.info("productName={}",productDto.getProductName());
+//        log.info("productPrice={}",productDto.getProductPrice());
+////        log.info("productQuantity={}",productDto.getProductQuantity());
+//        log.info("업로드위치={}",uploadDirectory);
+
+        //에러 발생시 addform으로 리다이렉트
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            model.addAttribute("errors", bindingResult);
+            return "/product/product-addform";
+        }
+
+        //새로운 product 저장 성공시 해당 product 상세페이지로 이동
+        try {
+            ProductDto saveProduct = productService.saveProduct(productSaveDto, productImages);
+            return "redirect:/products/" + saveProduct.getProductId();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IOException("IOException occurred while save product", e);//에러 발생시 예외로 던짐
+
+        }
+    }
 
     /*
     DB에서 product Id로 상품 조회, 상세페이지 형성
@@ -163,17 +171,12 @@ public class ProductController {
 //    @ResponseBody
     @GetMapping("/{id}")
     public String showProductDetail(@PathVariable Long id
-                                    ,Model model) throws MalformedURLException {
+            , Model model) throws MalformedURLException {
         //db에서 id로 아이템 조회 DTO 객체로 저장
         ProductDto productById = productService.findProductById(id);
         model.addAttribute(productById);
         //파일 이름 확인
-        //구현1 이미지 경로자체를 productDTo에 저장 - 수정하기 힘들다, 이미지를 받아오는 서비스계층에 넘기자
-//        log.info("첫번째 파일 경로={}",productById.getProductImagesPath().get(0));
-//        log.info("첫번째 파일 경로={}",productById.getProductImagesPath().get(1));
-        log.info("첫번째 파일 경로 ={}",productById.getProductImagesName().get(0));
-//        log.info("첫번째 파일 경로 ={}",productById.getProductImagesName().get(1));
-
+        log.info("첫번째 파일 경로 ={}", productById.getProductImagesName().get(0));
 
         //기능구현 1 - 첫번째 파일 이름 view에 전송
 //        UrlResource urlResource = new UrlResource("file:"+
@@ -192,25 +195,27 @@ public class ProductController {
     public ResponseEntity<Resource> serveProductImage(@PathVariable String imageName) throws MalformedURLException {
         Resource productImage = productService.getProductFileByFileName(imageName);
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_TYPE,MediaType.IMAGE_JPEG_VALUE)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_JPEG_VALUE)
                 .body(productImage);
 
-    };
+    }
+
+    ;
 
     /*/
     상품 수정 기능
      */
     @GetMapping("/{id}/update")
     public String showProductUpdateForm(@PathVariable Long id,
-                                            Model model){
+                                        Model model) {
 
         //DB에서 Id로 상품 조회
         ProductDto foundProductById = productService.findProductById(id);
-        //Model 정보 담아서 form에 전송, 수정form과 동일함
-        model.addAttribute("productDto",foundProductById);
-        model.addAttribute("productId",id);
+        //Model 정보 담아서 form에 전송
+        model.addAttribute("productDto", foundProductById);
+        model.addAttribute("productId", id);
 
-        log.info("productManufacturer={}",foundProductById.getProductManufacturer());
+        log.info("productManufacturer={}", foundProductById.getProductManufacturer());
 
         return "/product/product-updateform";
 
@@ -219,18 +224,16 @@ public class ProductController {
     @PostMapping("/{id}/update")
     public String updateProduct(@ModelAttribute ProductDto productDto,
                                 @PathVariable Long id,
-                                Model model){
+                                Model model) {
         //수정기능
-        productService.updateProductById(id,productDto);
+        productService.updateProductById(id, productDto);
 
         //수정된 상품 객체에 저장
         ProductDto productById = productService.findProductById(id);
         //해당 product 상세 페이지로 이동
         model.addAttribute(productById);
-        return "product/product-detail";
+        return "redirect:/products/" + productById.getProductId();
     }
-
-
 
 
     @GetMapping("/urltest")
@@ -241,7 +244,7 @@ public class ProductController {
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.IMAGE_JPEG);
-        return new ResponseEntity<>(urlResource,headers,HttpStatus.OK);
+        return new ResponseEntity<>(urlResource, headers, HttpStatus.OK);
 
     }
 
@@ -254,10 +257,10 @@ public class ProductController {
         //한글 깨짐 방지 UTF_8로 인코딩
         String encoeded = UriUtils.encode(fileName, StandardCharsets.UTF_8);
 
-        String contentDisposition = "attachment; filename=\""+
-                encoeded +"\"";
+        String contentDisposition = "attachment; filename=\"" +
+                encoeded + "\"";
 
-        return  ResponseEntity.ok()
+        return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
                 .body(urlResource);
     }
@@ -270,7 +273,6 @@ public class ProductController {
      */
 
     //application.properties 의 file.dir properties 값을 가져옴
-
 
 
     @GetMapping("/upload")
@@ -286,7 +288,7 @@ public class ProductController {
         log.info("itemName={}", itemName);
         log.info("multipartFile={}", file);
 
-        if(!file.isEmpty()){
+        if (!file.isEmpty()) {
             String fullPath = uploadDirectory + file.getOriginalFilename();
             log.info("fullPath={}", fullPath);
             file.transferTo(new File(fullPath));
@@ -295,12 +297,6 @@ public class ProductController {
 
         return "/product/product-upload-form";
     }
-
-
-
-
-
-
 
 
 }
